43cda34bc9f79a3ce897b03334da9eaa8a346eae
Matthew <dudukin.matvey@mail.ru>
2025-03-03 15:33:24 +0300

    add config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	JWTSecret     string
+	JWTExpiration time.Duration
+	DatabaseURL   string
+	ServerPort    string
+}
+
+func LoadConfig() (Config, error) {
+	if err := godotenv.Load(); err != nil {
+		return Config{}, err
+	}
+
+	return Config{
+		JWTSecret:     os.Getenv("JWT_SECRET"),
+		JWTExpiration: time.Hour * 24,
+		DatabaseURL:   os.Getenv("DATABASE_URL"),
+		ServerPort:    os.Getenv("SERVER_PORT"),
+	}, nil
+}
